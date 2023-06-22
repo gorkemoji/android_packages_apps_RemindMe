3994c2a9041c88fe4cbf793e0acf2fb4916cad50
Görkem <emircan9921@outlook.com>
2023-06-22 02:01:46 +0100

    Added Settings option for Dark Mode

--- app/src/main/java/com/gorkemoji/remindme/MainActivity.kt
+++ app/src/main/java/com/gorkemoji/remindme/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.gorkemoji.remindme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.gorkemoji.remindme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.settingsBtn.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_out_bottom, R.anim.slide_in_bottom)
+        }
     }
 }
--- app/src/main/java/com/gorkemoji/remindme/SettingsActivity.kt
+++ app/src/main/java/com/gorkemoji/remindme/SettingsActivity.kt
@@ -1,6 +1,8 @@
 package com.gorkemoji.remindme
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.gorkemoji.remindme.databinding.ActivitySettingsBinding
 
@@ -11,5 +13,20 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.darkModeSwitch.setOnCheckedChangeListener { compoundButton, isChecked ->
+            if (isChecked) {
+                // Do something when dark mode switch is being on.
+            }
+            else {
+                // Do something when dark mode switch is being off.
+            }
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        finish()
+        overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_bottom)
     }
 }
